0b48fb1eeb2c6a81a39605bd190cd203b75ad8fb
agentraghav <raghvendrarathore2@gmail.com>
2021-01-12 13:41:12 +0530

    data

---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    id: 1,
+    name: "Men's Round Neck T-shirt",
+    brand: 'Levis',
+    color: 'White',
+    price: 999,
+    imgsrc:
+      'https://g3fashion.com/upload/products/levis_white_hued_printed_t_shirt_1557552853as1328364_1.jpg',
+  },
+  {
+    id: 2,
+
+    name: 'Men Navy Slim Fit Solid Casual Shirt',
+    brand: 'Levis',
+    color: 'Navy Blue',
+    price: 1199,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9911451/2019/7/31/ec838275-487b-4f6e-b680-74ce183981e91564573884640-Levis-Men-Shirts-1601564573883359-1.jpg',
+  },
+  {
+    id: 3,
+    name: 'Men Blue Super Slim Fit Solid Casual Shirt',
+    brand: 'Peter England',
+    color: 'Blue',
+    price: 1199,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/13142232/2020/12/18/d07f3330-0725-43cb-bad3-64b9644b6bab1608287498735-PETER-ENGLAND-UNIVERSITY-Men-Shirts-7191608287497014-1.jpg',
+  },
+  {
+    id: 4,
+    name:
+      'Men Blue Slim Tapered Michael Fit Mid-Rise Clean Look Stretchable Jeans',
+    brand: 'Flying Machine',
+    color: 'Blue',
+    price: 2500,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12971096/2020/12/11/8ed339b6-a778-43db-b144-7b7a4d155fb81607667428948-Mens-Cotton-Blend-Slim-Blue-Jeans-7491607667426666-2.jpg',
+  },
+  {
+    id: 5,
+    name: 'Men Blue 513 Straight Fit Mid-Rise Clean Look Stretchable Jeans',
+    brand: 'Levis',
+    color: 'Blue',
+    price: 3000,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11165816/2020/10/7/921e2e12-bf92-49a2-bdac-60b42a3aaabf1602061502602-Levis-Men-Jeans-7191602061499538-1.jpg',
+  },
+  {
+    id: 6,
+    name: 'Men Black Slim Fit Mid-Rise Clean Look Stretchable Jeans',
+    brand: 'Highlander',
+    color: 'Black',
+    price: 2000,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2173989/2017/12/5/11512460656327-HIGHLANDER-Men-Black-Slim-Fit-Mid-Rise-Clean-Look-Stretchable-Jeans-7391512460656115-1.jpg',
+  },
+  {
+    id: 7,
+    name: 'Men White Solid Trainers',
+    brand: 'H & M',
+    color: 'White',
+    price: 1999,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2020/7/18/0a56543f-2680-4678-990e-c330d3ab22c41595031724557-1.jpg',
+  },
+  {
+    id: 8,
+    name:
+      'Men Navy Blue & White Colourblocked Leather Grip Mid IDP Mid-Top Sneakers',
+    brand: 'Puma',
+    color: 'Navy Blue',
+    price: 1799,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9433651/2019/4/26/af36affd-89de-454e-b34d-f92eaa0d7bd21556265229743-Puma-Men-Navy-Blue-Sneakers-611556265228796-1.jpg',
+  },
+  {
+    id: 9,
+    name: 'Men Off-White GAIMAN Sneakers',
+    brand: 'U.S. Polo Assn.',
+    color: 'White',
+    price: 1900,
+    imgsrc:
+      'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2019/9/23/3acbae82-1f4c-4903-a4f5-d23b888d05fa1569223902935-1.jpg',
+  },
+];
